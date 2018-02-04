1200be8879131bfe0c8ad0a1bb0634080e50ef1f
Martin Skeem <martinskeem@gmail.com>
2018-02-04 12:04:43 +0100

    added economics constants

---
+++ economics_constants.py
@@ -0,0 +1,14 @@
+roducts_url = 'https://restapi.e-conomic.com/products'
+product_url = 'https://restapi.e-conomic.com/products/{0}'
+customers_url = 'https://restapi.e-conomic.com/customers'
+customer_url = 'https://restapi.e-conomic.com/customers/{0}'
+invoice_url = 'https://restapi.e-conomic.com/invoices/drafts/{0}'
+invoices_url = 'https://restapi.e-conomic.com/invoices/drafts'
+
+customer_vatzone = 1
+customer_group = 3
+customer_paymentterms = 5
+invoice_layout = 18
+invoice_shipping_product_id = 'Fragt'
+invoice_vat_reverse_rate = .8
+product_group = 6000
